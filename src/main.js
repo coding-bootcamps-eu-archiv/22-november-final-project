10df9fa985b3ae0420a2e75f5b9d754c51dba147
Tim Kranich <tim@logyco.de>
2023-03-05 12:54:26 +0100

    feat: add font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import App from "./App.vue";
 import router from "./router";
 
 const pinia = createPinia();
 const app = createApp(App);
 
+library.add(faPen);
+library.add(faTrashCan);
+
 app.use(pinia);
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
